81ae14f97054674301030a7164c7a74b78deadb3
Shannon Grant <grant.shannon90@gmail.com>
2020-10-19 16:05:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,13 @@
 def key_for_min_value(name_hash)
   minimum_value = nil
   minimum_key = nil
+
   name_hash.each do |key, value|
     if minimum_value == nil
-      minimum_value = key
-      mini
+      minimum_value = value
+      minimum_key = key
+    elsif value < minimum_value
+      minimum_value = value
+      minimum_key = key
   end
 end
